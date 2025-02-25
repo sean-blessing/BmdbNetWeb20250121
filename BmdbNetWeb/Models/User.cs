99a38d183b8f3c6406b9bcb9528e9e8f1af5078e
Sean Blessing <snblessing@gmail.com>
2025-02-25 16:33:31 -0500

    UserLogin DTO demo

--- BmdbNetWeb/Models/User.cs
+++ BmdbNetWeb/Models/User.cs
@@ -1,39 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
-namespace BmdbNetWeb.Models;
+namespace BmdbNetWeb.Models {
+    [Table("User")]
+    public partial class User {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
 
-[Table("User")]
-[Index("Username", Name = "UQ__User__536C85E46C74725C", IsUnique = true)]
-public partial class User
-{
-    [Key]
-    public int Id { get; set; }
-
-    [StringLength(20)]
-    [Unicode(false)]
-    public string Username { get; set; } = null!;
-
-    [StringLength(10)]
-    [Unicode(false)]
-    public string Password { get; set; } = null!;
-
-    [StringLength(20)]
-    [Unicode(false)]
-    public string FirstName { get; set; } = null!;
-
-    [StringLength(20)]
-    [Unicode(false)]
-    public string LastName { get; set; } = null!;
-
-    [StringLength(12)]
-    [Unicode(false)]
-    public string PhoneNumber { get; set; } = null!;
-
-    [StringLength(75)]
-    [Unicode(false)]
-    public string Email { get; set; } = null!;
+    }
 }
